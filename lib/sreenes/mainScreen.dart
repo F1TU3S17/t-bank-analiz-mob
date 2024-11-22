4f6a33aad70f1d5ec43322b5ea91f67776b7019b
Artem <artemproris17@gmail.com>
2024-11-22 22:43:51 +0300

    viw

--- lib/sreenes/mainScreen.dart
+++ lib/sreenes/mainScreen.dart
@@ -23,19 +23,12 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
-<<<<<<< HEAD
     appBar: AppBar(
         title: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
             Center(child: const Text('Т-Аналитика', style: TextStyle(fontSize: 26))),
           ],
-=======
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset('../assets/logos/t.png'),
->>>>>>> master
         ),
       ),
       bottomNavigationBar: appNavigationBar(),
